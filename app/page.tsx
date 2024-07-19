aa15878afa598729e225e16afd49c212caf4f868
aarthi <aarthiamali@gmail.com>
2024-07-19 11:10:31 +0530

    h1 changed

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <h1>hello</h1>
+          <h1>Welcome to next js project</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
