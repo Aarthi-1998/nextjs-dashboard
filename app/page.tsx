1c8703a3819ef682bbf500ce11822aa4b43039b1
aarthi <aarthiamali@gmail.com>
2024-07-19 10:57:09 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <h1>hello</h1>
+          <h1>welcome to next js</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
